c3d5592de5a238a4c9eda1f89812947c579e3b05
Demi Wu <demiwu96@gmail.com>
2020-09-10 19:42:28 -0400

    start how are we different section

--- src/pages/about-css-modules.js
+++ src/pages/about-css-modules.js
@@ -8,7 +8,7 @@ const User = props => (
   <div className={styles.user}>
     <img src={props.avatar} className={styles.avatar} alt="" />
     <div className={styles.description}>
-      <h2 className={styles.username}>{props.username}kjlk</h2>
+      <h2 className={styles.username}>{props.username}</h2>
       <p className={styles.excerpt}>{props.excerpt}</p>
     </div>
   </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,27 @@
 import React from "react"
 import { Link } from "gatsby"
+import Cards from '../components/Cards';
 
 export default function Home() {
+  const testing = [{
+    title:'Client-determined goals',
+    contents:'We support the client in setting their own career, entrepreneurial or personal goals',
+  },{
+    title:'One-on-one skills development',
+    contents:'We do this through a tailored, project-based approach that meets unique goals and needs.',
+  },{
+    title:'Wrap-around supports',
+    contents: 'We have strategic partnerships with community organizations to provide additional support.',
+  },
+]
+
   return (
     <div style={{ color: `purple` }}>
       <Link to="/contact/">Contact</Link>
       <h1>Hello Gatsby!</h1>
       <p>What a world.</p>
       <img src="https://source.unsplash.com/random/400x200" alt="" />
+      <Cards cardContents={testing} />
     </div>
   )
 }
