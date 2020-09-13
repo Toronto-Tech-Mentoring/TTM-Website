321c23a9f54433c607815e55324ed3cbab89fa6e
Rishi Khan <rishfilet@pop-os.localdomain>
2020-09-13 19:03:44 -0400

    WIP: https://trello.com/c/rae74sDF Donate button. Just need to liink the heart and button to change colour together

--- src/components/Navbar/DonateBtn.js
+++ src/components/Navbar/DonateBtn.js
@@ -1,8 +1,32 @@
-import React from "react"
+import React from "react";
+import styled from "styled-components";
+import Heart from "./Heart"
 
 
-export default function DonateBtn() {
-  return (
-    <button className="donateBtn mt-auto mb-auto"> Donate </button>
-  )
+// function Heart() {
+//   return (<img className="heartIcon" src={this.state.imgSrc} onMouseOver={this.handleMouseOver} onMouseOut={this.handleMouseOut}/>)
+// }
+
+const StyledButton = styled.button`
+  background: #873fe2;
+  border-radius: 24px;
+  border-style: none;
+  width: 156px;
+  height: 48px;
+  margin: 8px;
+  color: #fff;
+
+  &:hover {
+    background: #ffff;
+    border-style: solid;
+    border-width: medium;
+    border-color: #873fe2;
+    color: #873fe2;
+  }
+`
+
+function DonateBtn() {
+  return <StyledButton> Donate <Heart/></StyledButton>
 }
+
+export default DonateBtn
