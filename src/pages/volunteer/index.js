797f99e60ca7eba5811747ae90f31153e60a2e9f
gh0stl0nelyGit <khoinguyen19971997@gmail.com>
2020-09-13 18:59:55 -0400

    Test layout

---
+++ src/pages/volunteer/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Container from "../../components/container"
+import { MainLayout } from "../../layout";
+
+export default function VolunteerPage() {
+  return (
+    <MainLayout>
+      <p>This is Volunteer Page</p>
+    </MainLayout>
+  )
+}
