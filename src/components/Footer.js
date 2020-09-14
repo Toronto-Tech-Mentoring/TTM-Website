cf9150e46c96bf7a14e6f8b0e6c97f18cad5ffbb
Demi Wu <demiwu96@gmail.com>
2020-09-14 18:28:03 -0400

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -21,12 +21,12 @@ const useStyles = makeStyles((theme) => ({
         paddingRight: '80px',
         paddingTop: '20px',
         [theme.breakpoints.down('md')]: {
-            paddingRight: '20px',
+            paddingRight: '10px',
         },
         [theme.breakpoints.down('sm')]: {
             display: 'block',
-            paddingLeft: '20px',
-            margin: '0px'
+            margin: '0px',
+            paddingLeft:'20px',
         },
     },
     iconUl: {
@@ -35,7 +35,7 @@ const useStyles = makeStyles((theme) => ({
         height: '100%',
         marginBottom: '15px',
         [theme.breakpoints.down('sm')]: {
-            paddingLeft: '10px',
+            paddingLeft: '3vw',
             paddingRight: '0px'
         },
     },
@@ -43,8 +43,11 @@ const useStyles = makeStyles((theme) => ({
         display: 'inline-block',
         padding: '0px 10px',
         margin: '0',
+        [theme.breakpoints.down('md')]: {
+            padding: '0px 5px',
+        },
         [theme.breakpoints.down('xs')]: {
-            display: 'block'
+            display: 'block',
         },
     },
     linkText: {
@@ -61,20 +64,27 @@ const useStyles = makeStyles((theme) => ({
     },
     copyrightDiv: {
         float: 'left',
-        paddingLeft: '3vw',
+        paddingLeft: '100px',
         fontFamily: 'Poppins',
         alignItems: 'center',
         fontSize: '16px',
         paddingTop: '20px',
+        [theme.breakpoints.down('md')]: {
+            paddingLeft: '10px',
+        },
         [theme.breakpoints.down('xs')]: {
             paddingLeft: '20px',
             fontSize: '14px',
-            margin: '0px'
+            margin: '0px',
+            paddingTop: '5px',
         },
     },
     iconLi: {
         display: 'inline-block',
         padding: '12px 10px 0px 10px',
+        [theme.breakpoints.down('xs')]: {
+            padding: '12px 2px 0px 2px',
+        },
     },
     iconDiv: {
         float: 'right',
@@ -97,6 +107,13 @@ const useStyles = makeStyles((theme) => ({
     hideIcon: {
         [theme.breakpoints.between('md', 'xl')]: {
             display: 'none'
+        },
+    },
+    copyright:{
+        [theme.breakpoints.down('xs')]: {
+            position:'relative',
+            bottom:'8px',
+            fontSize:'15px'
         },
     }
 }));
@@ -146,6 +163,13 @@ export default function Footer() {
                         </a>
                     </li>
                     <li className={classes.iconLi}>
+                        <a href="#">
+                            <Avatar className={classes.iconAvatar}>
+                                <InstagramIcon />
+                            </Avatar>
+                        </a>
+                    </li>
+                    <li className={classes.iconLi}>
                         <a href="https://twitter.com/?lang=en">
                             <Avatar className={classes.iconAvatar}>
                                 <TwitterIcon />
@@ -160,11 +184,18 @@ export default function Footer() {
                 </ul>
             </div>
             <div className={classes.copyrightDiv}>
-                <span>&copy; 2020 Toronto Tech Mentoring</span>
+                <span className={classes.copyright}>&copy; 2020 Toronto Tech Mentoring</span>
                 <Hidden smUp>
                     <ul className={classes.iconUl}>
                         <li className={classes.iconLi}>
                             <a href="https://www.instagram.com/">
+                                <Avatar className={classes.iconAvatar}>
+                                    <InstagramIcon />
+                                </Avatar>
+                            </a>
+                        </li>
+                        <li className={classes.iconLi}>
+                            <a href="#">
                                 <Avatar className={classes.iconAvatar}>
                                     <InstagramIcon />
                                 </Avatar>
