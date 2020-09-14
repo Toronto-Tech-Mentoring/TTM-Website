5176eb8a45d412faf811a945a674f47cccee645e
Demi Wu <demiwu96@gmail.com>
2020-09-13 20:05:49 -0400

    add footer

--- src/layout/MainLayout.js
+++ src/layout/MainLayout.js
@@ -1,27 +1,13 @@
 import React from 'react';
 import NavBar from '../components/Navbar';
+import Footer from '../components/Footer';
 
-const styles = {
-    footer : {
-        position: 'fixed',
-        width: '1440px',
-        height: '80px',
-        left: '0px',
-        bottom: '0px',
-        background:' #FFFFFF',
-        marginTop: '100px',
-        zIndex: '999',
-    }
-}
-
-export default function MainLayout({children}){
+export default function MainLayout({ children }) {
     return (
-        <>
+        <div>
             <NavBar />
             {children}
-            <footer style={styles.footer}>
-                <h1>This is placeholder for Footer</h1>
-            </footer>
-        </>
+            <Footer />
+        </div>
     )
 }
