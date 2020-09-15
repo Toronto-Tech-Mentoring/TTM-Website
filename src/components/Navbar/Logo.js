3a4eafebe28eb0e2a2df2062c8256b6f12f25cf5
Brice Boutet <boutetlb@gmail.com>
2020-09-15 17:29:31 -0400

    Code clean-up &  comment out components/container.js

--- src/components/Navbar/Logo.js
+++ src/components/Navbar/Logo.js
@@ -5,7 +5,6 @@ import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles({
   root: {
     height: "80px",
-    display: "inline",
     display: "flex",
     flexWrap: "wrap",
     alignItems: "center"
