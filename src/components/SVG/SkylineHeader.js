0444a77b90b040524712a799b33d9b0622f749ac
Rishi Kapoor <airshiprook@gmail.com>
2020-09-16 18:50:04 -0400

    WIP: https://trello.com/c/6ItByRnR/46-3back-skyline-and-text-in-front-of-it x

--- src/components/SVG/SkylineHeader.js
+++
@@ -1,6 +0,0 @@
-import React from "react"
-import skylinehead from "../../images/skyline/skylineheader.svg"
-
-export default function SkylineHeaderSVG() {
-  return <img style={{ width: "100%" }} alt="city" src={skylinehead} />
-}
--- src/components/SVG/index.js
+++
@@ -1,2 +0,0 @@
-export { default as SkylineHeader } from "./SkylineHeader"
-export { default as SkylineFooter } from "./SkylineFooter"
