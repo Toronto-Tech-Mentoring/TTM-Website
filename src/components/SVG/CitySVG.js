bb9fcaed97ec90832164fa826a7360bba67268ae
Rishi Kapoor <airshiprook@gmail.com>
2020-09-16 12:53:25 -0400

    1. Renamed the CitySVG and CitySVG2 files to Skylinefooter and Skylineheader respectively. 2. Moved the skyline SVG files to a folder named skyline in images directory. 3. Renamed CitySVG.js and CitySVG2.js to SkylineFooter.js and SkylineHeader.js respectively. 4. Made the necessary name changes to the codebase in those files to reflect the new names.

--- src/components/SVG/CitySVG.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import citySVG from '../../images/about-us/How-are-we-different-section/city.svg';
-
-export default function CitySVG(){
-    return <img style={{width: '100%'}} alt="city" src={citySVG} />
-}
