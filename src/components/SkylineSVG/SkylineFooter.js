0cd4c83336b7ec5be4bee99107f3f35fd711accf
Rishi Kapoor <airshiprook@gmail.com>
2020-09-16 12:55:10 -0400

    Renamed src/components/SVG to src/components/SkylineSVG

---
+++ src/components/SkylineSVG/SkylineFooter.js
@@ -0,0 +1,6 @@
+import React from "react"
+import skylinefooter from "../../images/skyline/skylinefooter.svg"
+
+export default function SkylineFooterSVG() {
+  return <img style={{ width: "100%" }} alt="city" src={skylinefooter} />
+}
