3a14be85db68019584a7d4e1e4e53a526b361e88
gh0stl0nelyGit <khoinguyen19971997@gmail.com>
2020-09-15 20:23:07 -0400

    Commit change

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,13 @@
 import React from "react"
 import { MainLayout } from "../layout";
 import { HowAreWeDifferentSection, TheBottomLineSection, OurRootsSection } from "../components/Home";
-import { CitySVG } from "../components/SVG";
+import { CitySVG, CitySVG2 } from "../components/SVG";
 
-export default function Home() {
+export default function Home(){
   return (
     <MainLayout>
       <p>This is Home (About-us) page</p>
-      <CitySVG />
+      <CitySVG2 />
       <HowAreWeDifferentSection />
       <TheBottomLineSection />
       <OurRootsSection />
