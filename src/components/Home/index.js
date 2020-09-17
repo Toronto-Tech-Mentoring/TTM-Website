76d5ef54cc3764375440073cf9c39fc4b721dc18
Harsh Chawla <harshvchawla@yahoo.co.in>
2020-09-17 22:29:44 +0530

    https://trello.com/c/EsWwXbKw/132-6-values-driven-text-and-all-image-components - Added the values-driven text bubble

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,4 +1,6 @@
 export { default as HowAreWeDifferentSection } from './HowAreWeDifferentSection';
 export { default as TheBottomLineSection } from './TheBottomLineSection';
 export { default as OurRootsSection } from './OurRootsSection';
-export {default as TimelineSection} from './TimelineSection';
+export { default as TimelineSection } from './TimelineSection';
+export { default as WhyTechSection } from './WhyTechSection';
+export { default as WeAreValuesDrivenSection } from './WeAreValuesDrivenSection';
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,11 @@
 import React from "react"
 import { MainLayout } from "../layout";
-import { HowAreWeDifferentSection, TheBottomLineSection, OurRootsSection, TimelineSection } from "../components/Home";
+import { HowAreWeDifferentSection, TheBottomLineSection, OurRootsSection, TimelineSection, WhyTechSection, WeAreValuesDrivenSection } from "../components/Home";
 import { CitySVG as SecondBackgroundCitySVG, CitySVG2 as FirstBackgroundCitySVG} from "../components/SVG";
-import LeftAnchoredBubbleText from '../components/Home/LeftAnchoredBubbleText'
 
 export default function Home(){
   return (
     <MainLayout>
-      {/* <LeftAnchoredBubbleText/> */}
       <FirstBackgroundCitySVG />  
       {/* @Hanlin and @Saint -> The line of code is the back city SVG.
         Feel free to use it or replace it with your own. 
@@ -17,6 +15,10 @@ export default function Home(){
       <OurRootsSection />
       <TimelineSection />
       <SecondBackgroundCitySVG />
+
+       <WhyTechSection/>
+       <WeAreValuesDrivenSection/>
+
     </MainLayout>
   )
 }
