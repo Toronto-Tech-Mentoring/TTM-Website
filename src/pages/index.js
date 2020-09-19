7ca7a131b604d52145d8752baaeec40f3af269cd
Hanlin Cheng <hanlin.cheng@uwaterloo.ca>
2020-09-19 00:41:56 +0000

    Fixed heading font, fixed font and padding for header text

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,9 +19,6 @@ export default function Home() {
     <MainLayout>
       {/* <LeftAnchoredBubbleText/> */}
       <SkylineHeaderSVG />
-      {/* @Hanlin and @Saint -> The line of code is the back city SVG.
-        Feel free to use it or replace it with your own. 
-       */}
       <HeroText
         heroText="What is Toronto Tech Mentoring?"
         heroBodyText="We aim to empower homeless and underhoused individuals in Toronto through technology skills, mentorship and other learning supports to achieve professional, entrepreneurial or personal goals. Our core program delivers mentorship for marginalized young people (including LGBTQ+)"
