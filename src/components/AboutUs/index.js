954e010cfb3a7b59179a1ab4bb887d57e50a3632
Brice Boutet <boutetlb@gmail.com>
2020-09-21 21:23:47 -0400

    Updating the Skyline components imports

--- src/components/AboutUs/index.js
+++ src/components/AboutUs/index.js
@@ -5,22 +5,27 @@ import TheBottomLineSection  from './TheBottomLineSection';
 import OurRootsSection from './OurRootsSection';
 import TimelineSection from './TimelineSection';
 import LeftAnchoredBubbleText from './LeftAnchoredBubbleText'
-import { CitySVG as SecondBackgroundCitySVG, CitySVG2 as FirstBackgroundCitySVG} from "../SVG";
+import {
+  SkylineHeader as SkylineHeaderSVG,
+  SkylineFooter as SkylineFooterSVG,
+} from "../SkylineSVG"
+import HeroText from "../HeroText/HeroText"
 
 
-export default function Home(){
+export default function AboutUs() {
   return (
-    <div>
+    <MainLayout>
       {/* <LeftAnchoredBubbleText/> */}
-      <FirstBackgroundCitySVG />  
-      {/* @Hanlin and @Saint -> The line of code is the back city SVG.
-        Feel free to use it or replace it with your own. 
-       */}
-      <HowAreWeDifferentSection /> 
+      <SkylineHeaderSVG />
+      <HeroText
+        heroText="What is Toronto Tech Mentoring?"
+        heroBodyText="We aim to empower homeless and underhoused individuals in Toronto through technology skills, mentorship and other learning supports to achieve professional, entrepreneurial or personal goals. Our core program delivers mentorship for marginalized young people (including LGBTQ+)"
+      />
+      <HowAreWeDifferentSection />
       <TheBottomLineSection />
       <OurRootsSection />
       <TimelineSection />
-      <SecondBackgroundCitySVG />
-    </div>
+      <SkylineFooterSVG />
+    </MainLayout>
   )
 }
