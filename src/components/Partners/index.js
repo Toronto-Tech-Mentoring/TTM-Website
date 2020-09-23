c55c20e512604a647ca5a6d73fda79898d87d319
gh0stl0nelyGit <khoinguyen19971997@gmail.com>
2020-09-23 18:10:13 -0400

    Perfected Last section for Partner page

--- src/components/Partners/index.js
+++ src/components/Partners/index.js
@@ -4,14 +4,9 @@ import LastSection from "./LastSection";
 export default function Partners() {
   return (
   <div>
-<<<<<<< HEAD
       <p style={{padding:"10px", margin:"20px", backgroundColor:"yellow", textAlign:"center"}}>
          I am <strong>PARTNERS</strong> page's body.</p>
       <LastSection />
   </div>
-=======
-       <h1>Partners page</h1>
-    </div>
->>>>>>> master
   )
 }
