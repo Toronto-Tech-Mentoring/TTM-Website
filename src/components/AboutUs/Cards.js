8febd04d5d7e4a5ef3ddc3a3c2f9a6c657f706a8
David Ghazi <davidaghazi@gmail.com>
2020-09-27 22:09:05 -0400

    updated responsiveness for card/how-are-we-different - https://trello.com/c/uRxDNHRv

--- src/components/AboutUs/Cards.js
+++ src/components/AboutUs/Cards.js
@@ -16,12 +16,12 @@ const useStyles = makeStyles((theme) => ({
   root: {
     display: 'inline-block',
     maxWidth: '350px',
-    // maxHeight: '325px',
+    maxHeight: '360px',
     borderRadius: '30px',
     margin: '0px 25px',
-    marginTop: '-5px',
+    marginTop: '15px',
     [theme.breakpoints.down('md')]: {
-      margin: '25px 15px',
+      margin: '25px 25px',
     },
   },
   media: {
@@ -39,7 +39,6 @@ const useStyles = makeStyles((theme) => ({
     },
     [theme.breakpoints.down('md')]: {
       fontSize: '18px',
-      fontWeight: 600,
     },
   },
   contents: {
@@ -65,21 +64,21 @@ export default function Cards() {
   const classes = useStyles();
 
   return (
-    <>
-      <Grid item xs={12} md={4}>
+    <Grid container alignItems="center" justify="center">
+      <Grid item xs={12} md={4} lg={3} xl={2.5}>
         <Card className={classes.root}>
           <p className={classes.title}>Client-determined goals</p>
           <div className={classes.imgDiv} style={{ backgroundImage: `url(${background1}` }}>
             <img className={classes.innerImage} style={{ top: '35px' }} alt="skill_development" src={pic1} />
-          </div>
+          </div> 
           <CardContent>
             <p className={classes.contents}>
-              We support the client in setting their own career, entrepreneurial or personal.
+              We support the client in setting their own career, entrepreneurial or personal goals.
             </p>
           </CardContent>
         </Card>
       </Grid>
-      <Grid item xs={12} md={4}>
+      <Grid item xs={12} md={4} lg={3} xl={2.5}>
         <Card className={classes.root}>
           <p className={classes.title}>One-on-one skills development</p>
           <div className={classes.imgDiv} style={{ backgroundImage: `url(${background2}` }}>
@@ -93,20 +92,20 @@ export default function Cards() {
           </CardContent>
         </Card>
       </Grid>
-      <Grid item xs={12} md={4}>
+      <Grid item xs={12} md={4} lg={3} xl={2.5}>
         <Card className={classes.root}>
           <p className={classes.title}>Wrap-around supports</p>
           <div className={classes.imgDiv} style={{ backgroundImage: `url(${background3}` }}>
             <img className={classes.innerImage} style={{ top: '44px', zIndex: '3', left: '60px' }} alt="supports" src={pic3} />
             <img className={classes.innerImage} style={{ top: '33px', zIndex: '2', right: '60px' }} alt="shiny" src={shiny3} />
           </div>
-          <CardContent>
+          <CardContent> 
             <p className={classes.contents}>
               We have strategic partnerships with community organizations to provide additional support.
             </p>
           </CardContent>
         </Card>
       </Grid>
-    </>
+    </Grid>
   );
 }
