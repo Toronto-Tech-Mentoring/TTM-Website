7b138b8b74ec6b7c42bfe6de3dc57b1a43761f54
David Ghazi <davidaghazi@gmail.com>
2020-10-02 20:06:00 -0400

    cleaned up code in files

--- src/components/WhyTechSVG/ValuesDriven.js
+++ src/components/WhyTechSVG/ValuesDriven.js
@@ -1,9 +1,10 @@
 import React from "react";
+
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
 
-
 const useStyles = makeStyles((theme) => ({ 
+    
     root: {
         [theme.breakpoints.down('xl')]: {
             paddingLeft: 450
--- src/components/WhyTechSVG/WhyTech.js
+++ src/components/WhyTechSVG/WhyTech.js
@@ -1,9 +1,10 @@
 import React from "react";
+
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
 
-
 const useStyles = makeStyles((theme) => ({ 
+    
     cssHeaderText: {
         fontFamily: 'Josefin Sans',
         fontStyle: 'normal',
