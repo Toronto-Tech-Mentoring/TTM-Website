7222536ecec06809b40c0a720c0832e8d3072d52
Demi Wu <demiwu96@gmail.com>
2020-10-06 21:04:43 -0400

    responsive why tech and values driven section

--- src/components/AboutUs/index.js
+++ src/components/AboutUs/index.js
@@ -4,10 +4,7 @@ import HowAreWeDifferentSection from './HowAreWeDifferentSection';
 import TheBottomLineSection  from './TheBottomLineSection';
 import OurRootsSection from './OurRootsSection';
 import TimelineSection from './TimelineSection';
-import ValuesDriven from '../WhyTechSVG/ValuesDriven';
-import WhyTech from '../WhyTechSVG/WhyTech';
-import Computer from '../WhyTechSVG/ComputerSVG';
-import Hands from '../WhyTechSVG/HandsSVG';
+import WhyTechAndValuesDrivenSection from '../WhyTechSVG/index';
 import {
   SkylineHeader as SkylineHeaderSVG,
   SkylineFooter as SkylineFooterSVG,
@@ -23,12 +20,7 @@ export default function Home() {
         heroText="What is Toronto Tech Mentoring?"
         heroBodyText="We aim to empower homeless and underhoused individuals in Toronto through technology skills, mentorship and other learning supports to achieve professional, entrepreneurial or personal goals. Our core program delivers mentorship for marginalized young people (including LGBTQ+)"
       />
-      <WhyTech />
-      <Hands />
-      <ValuesDriven/>
-      <HowAreWeDifferentSection />
-      <TheBottomLineSection />
-      <Computer />
+      <WhyTechAndValuesDrivenSection />
       <OurRootsSection />
       <TimelineSection />
       <SkylineFooterSVG />
