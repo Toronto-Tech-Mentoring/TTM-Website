c3b66bcecf3bfb5cc4449adee3f06278040ac274
Hanlin Cheng <hanlin.cheng@uwaterloo.ca>
2020-10-16 22:48:43 -0400

    fixed padding for large screen size

--- src/components/AboutUs/TimelineSection.js
+++ src/components/AboutUs/TimelineSection.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { makeStyles, StylesProvider } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 
 import TitleRight from '../../images/about-us/bottom-line/titleRight.svg';
@@ -9,8 +9,6 @@ import Pic1 from '../../images/about-us/timeline/pic1.svg';
 import Pic3 from '../../images/about-us/timeline/pic3.svg';
 import LeftBlob from '../../images/about-us/timeline/leftblob.svg';
 import RightBlob from '../../images/about-us/timeline/rightblob.svg';
-import { DriveEtaOutlined } from "@material-ui/icons";
-
 const useStyles = makeStyles((theme) => ({
     title: {
         fontFamily: 'Josefin Sans',
@@ -22,7 +20,6 @@ const useStyles = makeStyles((theme) => ({
         [theme.breakpoints.between('sm', 'md')]: {
             fontSize: '28px',
         },
-
         // Tablet below to Mobile
         [theme.breakpoints.down('sm')]: {
             fontSize: '18px',
@@ -107,7 +104,7 @@ const useStyles = makeStyles((theme) => ({
     },
     pic: {
         display: 'inline-block',
-        width: '350px',
+        width: '243px',
         borderRadius: '10px',
 
         // Tablet Above to Laptop/ Desktop
@@ -188,24 +185,20 @@ export default function TimelineSection() {
     return (
       <Grid container justify="center" alignItems="center">
         <Grid item xs={12} className={classes.center}>
-          {/* <img alt="title decoration left" src={titleLeft} /> */}
           <TitleLeft />
           <span className={classes.title}>See how we’ve grown!</span>
           <TitleRight />
-          {/* <img alt="title decoration right" src={titleRight} /> */}
         </Grid>
         <Grid className={classes.gridContainer} container item xs={12}>
           <Grid item xs={2} md={3} lg={3} className={classes.center}>
             <span className={classes.yearTag}>2017</span>
             <Pin className={classes.pinStyle} />
-            {/* <img className={classes.pinStyle} alt="pin" src={pin} /> */}
           </Grid>
           <Grid container item xs={10} md={9} lg={9}>
             <span className={classes.yearTagSm}>2017</span>
             <span className={classes.eventTitle}>
               Research and program development
             </span>
-            {/* <LeftBlob style = {StylesProvider.overlay/> */}
             <p className={classes.eventContent}>
               Toronto Tech Mentoring was born out of the Civic Tech Toronto
               space. Civic Tech is a movement that focuses on developing and
@@ -214,10 +207,8 @@ export default function TimelineSection() {
             <div>
               <Pic1
                 className={`${classes.firstPic} ${classes.pic}`}
-                // src={pic1}
               />
               <Pic1 className={classes.pic}/>
-              {/* <img className={classes.bg} alt="purple bubble" src={bg3} /> */}
             </div>
           </Grid>
         </Grid>
@@ -225,8 +216,6 @@ export default function TimelineSection() {
           <Grid item xs={2} md={3} lg={3} className={classes.center}>
             <span className={classes.yearTag}>2018</span>
             <Pin className={classes.pinStyle} />
-
-            {/* <img className={classes.pinStyle} alt="pin" src={pin} /> */}
           </Grid>
           <Grid container item xs={10} md={9} lg={9}>
             <span className={classes.yearTagSm}>2018</span>
@@ -241,10 +230,8 @@ export default function TimelineSection() {
             <div>
               <Pic3
                 className={`${classes.firstPic} ${classes.pic}`}
-                // src={pic3}
               />
               <Pic3 className={classes.pic}/>
-              {/* <img className={classes.bg} alt="blue bubble" src={bg2} /> */}
             </div>
           </Grid>
         </Grid>
@@ -252,8 +239,6 @@ export default function TimelineSection() {
           <Grid item xs={2} md={3} lg={3} className={classes.center}>
             <span className={classes.yearTag}>2019</span>
             <Pin className={classes.pinStyle} />
-
-            {/* <img className={classes.pinStyle} alt="pin" src={pin} /> */}
           </Grid>
           <Grid item xs={10} md={9} lg={9}>
             <span className={classes.yearTagSm}>2019</span>
@@ -264,7 +249,6 @@ export default function TimelineSection() {
               nisi tristique viverra at ac elit.
             </p>
             <Grid item xs={12}>
-              {/* <img className={classes.bg} alt="orange bubble" src={bg3} /> */}
             </Grid>
           </Grid>
         </Grid>
