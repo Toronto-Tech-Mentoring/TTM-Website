43171dbabbebad3df422791b217ca9595fd3a9f8
Brice Boutet <boutetlb@gmail.com>
2020-10-22 03:10:25 -0400

    Responsive contact button size

--- src/components/Sponsors/DonateIconsGroup.js
+++ src/components/Sponsors/DonateIconsGroup.js
@@ -4,17 +4,21 @@ import DonateIcons from '../../images/sponsors/donateIcons.svg';
 
 const useStyles = makeStyles((theme) => ({
     image: {
-        position: 'fixed',
-        right: 'calc(14.7% - 94px)',
-        bottom : '97px',
+        rightMargin: 'calc(14.7% - 94px)',
+        bottomMargin : '97px',
         width: 'calc(34.2vw - 19px)',
         height: 'auto'
     },
         // Tablet to Mobile
         [theme.breakpoints.down('md')]: {
-            right: 'calc(1% + 11px)',
-            bottom : '97px',
+            rightMargin: 'calc(1% + 11px)',
+            bottomMargin : '97px',
             width: 'calc(26vw + 47px)',
+    },
+         [theme.breakpoints.down('sm')]: {
+            rightMargin: 'calc(1% + 11px)',
+            bottomMargin : '97px',
+            width: 'auto',
         },
 }));
 
--- src/components/Sponsors/DonateTodaySection.js
+++ src/components/Sponsors/DonateTodaySection.js
@@ -1,30 +1,50 @@
 import React from "react";
-import Grid from '@material-ui/core/Grid';
+import {Box, Container, makeStyles} from '@material-ui/core';
 
-import GridContainer from '../shared/GridContainer'
-import TextBox from '../shared/TextBox'
 import Title from '../shared/Title'
 import PContent from '../shared/ParagraphedContent'
 import ContactButton from '../shared/ContactButton'
 import DonateIconsGroup from './DonateIconsGroup'
 
-export default function TheBottomLineSection() {
+const useStyles = makeStyles((theme) => ({
 
+     container: {
+        height: 'calc(40vw + 134px)',
+        marginTop: 'calc(7vw + 94px)',
+         padding:'0px'
+       
+    },
+    textBox: {
+        float: 'left',
+        marginLeft: 'calc(16vw - 94px)',
+        width: 'calc(27vw + 198px)',
+           [theme.breakpoints.down('xs')]: {
+            width: '100%',
+        }
+    },
+
+    button: {
+       width: 'calc(4vw + 122px)' 
+    }
+
+}));
+
+
+export default function TheBottomLineSection() {
+    const classes = useStyles();
     const title = "Donate Today"
     const content = "Help us continue to give young people a chance to access tech skills and change their lives."
     const buttonText = "Donate"
 
     return (
-        <GridContainer>
-            <Grid item xs={12} md={6}>
-            <TextBox>
-                <Title title={title} hideCheetohs={["xl","lg","md","sm","xs"]}/>
+        <Container className={classes.container}>
+            <Box className={classes.textBox}>
+                <Title title={title} hideCheetohs={["xl","lg","md","sm","xs"]} />
                 <PContent content={content}/>
-                <ContactButton buttonText={buttonText}/>
-            </TextBox>
-            </Grid>
+                <ContactButton buttonText={buttonText} className={classes.button}/>
+            </Box>
             <DonateIconsGroup />
-        </GridContainer>
+        </Container>
     )
 };
 
