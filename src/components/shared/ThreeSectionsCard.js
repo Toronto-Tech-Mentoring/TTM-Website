d83233c4706604f48e8909dae22280285167e737
Brice Boutet <boutetlb@gmail.com>
2020-10-29 23:23:39 -0400

    Update ThreeSectionsCard.js
    Removed console.log

--- src/components/shared/ThreeSectionsCard.js
+++ src/components/shared/ThreeSectionsCard.js
@@ -3,7 +3,6 @@ import { makeStyles } from '@material-ui/core';
 
 
 export default function ImageCard(props) {
-    console.log(props.card.style)
     const useStyles = makeStyles(
         props.card.style,
 );
