e24690eeb184d9057c7ae9731c0b2bcccd8049be
Brice Boutet <boutetlb@gmail.com>
2020-11-18 22:41:04 -0500

    Depracated gatsby-plugin-prefetch-google-fonts replacement

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,38 +5,36 @@
  */
 
 module.exports = {
-  /* Your site config here */
-  plugins: [
-    {
-      resolve: `gatsby-plugin-prefetch-google-fonts`,
-      options: {
-        fonts: [
-          {
-            family: `Josefin Sans`,
-            variants: [`300`, `400`, `500`, `600`, `700`, `800`, `900`],
-          },
-          {
-            family: `Poppins`,
-            variants: [`300`, `400`, `500`, `600`, `700`, `800`, `900`],
-          },
-        ],
-      },
-    },
-    {
-      resolve: "gatsby-plugin-react-svg",
-      options: {
-        rule: {
-          include: /(about-us|skyline|timeline|bottom-line|partner-organization|vital-role|cheetohs)/,
+    /* Your site config here */
+    plugins: [{
+            resolve: `gatsby-plugin-google-fonts`,
+            options: {
+                fonts: [{
+                        family: `Josefin Sans`,
+                        variants: [`300`, `400`, `500`, `600`, `700`, `800`, `900`],
+                    },
+                    {
+                        family: `Poppins`,
+                        variants: [`300`, `400`, `500`, `600`, `700`, `800`, `900`],
+                    },
+                ],
+            },
         },
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/src/images/nonsvgs`,
-      },
-    },
-  ],
+        {
+            resolve: "gatsby-plugin-react-svg",
+            options: {
+                rule: {
+                    include: /(about-us|skyline|timeline|bottom-line|partner-organization|vital-role|cheetohs)/,
+                },
+            },
+        },
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                path: `${__dirname}/src/images/nonsvgs`,
+            },
+        },
+    ],
 }
