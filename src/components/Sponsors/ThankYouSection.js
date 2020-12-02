680a4934583b5a74cced507c587585f1f3d0196a
David Ghazi <davidaghazi@gmail.com>
2020-12-02 12:34:38 -0500

    completed thankyou section in sponsors page

--- src/components/Sponsors/ThankYouSection.js
+++ src/components/Sponsors/ThankYouSection.js
@@ -7,15 +7,64 @@ import Hands from '../../images/sponsors/thankyou/hands.svg'
 import Stars from '../../images/sponsors/thankyou/stars.svg'
 
 const useStyles = makeStyles((theme) => ({
+    container: {
+        height: '648px',
+        width: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        [theme.breakpoints.down('769')]: {
+            height: '614px'
+        },
+        [theme.breakpoints.down('426')]: {
+            height: '627px'
+        },
+    },
+    starsStyle: {
+        position: 'relative',
+        marginTop: 'calc(2vw - 100px)',
+        [theme.breakpoints.down('1300')]: {
+            marginTop: 'calc(5vw - 150px)'
+        },
+        [theme.breakpoints.down('1025')]: {
+            marginTop: 'calc(10vw - 200px)'
+        },
+        [theme.breakpoints.down('769')]: {
+            marginTop: 'calc(6vw - 200px)'
+        },
+        [theme.breakpoints.down('700')]: {
+            marginTop: 'calc(10vw - 300px)'
+        },
+        [theme.breakpoints.down('426')]: {
+            marginTop: 'calc(10vw - 185px)',
+            marginRight: 'calc(2vw - 50px)'
+        },
+    },
     blobStyle: {
-        marginTop: '78px',
+        position: 'absolute',
         width:"1056px", 
-        height:"344px", 
-        [theme.breakpoints.down('sm')]: {
-            position: 'relative',
-            width:"849px", 
-            height:"276px",
-          },
+        height:"344px",
+        marginTop: 'calc(2vw - 150px)',
+        [theme.breakpoints.down('1300')]: {
+            marginTop: 'calc(2vw - 150px)'
+        },
+        [theme.breakpoints.down('1025')]: {
+            marginTop: 'calc(2vw - 150px)'
+        },
+        [theme.breakpoints.down('550')]: {
+            marginTop: 'calc(2vw - 100px)',
+        },
+        [theme.breakpoints.down('426')]: {
+            visibility: 'hidden'
+        },
+    },
+    mobileBlobStyle: {
+        visibility: 'hidden',
+        [theme.breakpoints.down('426')]: {
+            marginTop: 'calc(2vw - 180px)',
+            visibility: 'visible',
+            width: '100%',
+        },
     },
     headerText: {
         fontFamily: 'Josefin Sans',
@@ -26,6 +75,10 @@ const useStyles = makeStyles((theme) => ({
         display: 'flex',
         alignItems: 'center',
         color: '#000000',
+        [theme.breakpoints.down('426')]: {
+            fontSize: '20px',
+            lineHeight: '32px'
+        },
     },
     bodyText: {
         fontFamily: 'poppins',
@@ -34,52 +87,35 @@ const useStyles = makeStyles((theme) => ({
         fontSize: '18px',
         lineHeight: '31px',
         display: 'flex',
-        alignItems: 'center',
-        color: '#000000'
+        align: 'center',
+        color: '#000000',
+        [theme.breakpoints.down('426')]: {
+            position: 'absolute',
+            fontSize: '14px',
+            lineHeight: '24px'
+        },
     },
     handsStyle: {
-        // position: 'relative',
-        [theme.breakpoints.down('xl')]: {
-            margin: '-300px 0px 0px 700px',
-            // marginTop: '89px',
-            // backgroundColor: 'blue'
+        position: 'absolute',
+        right: 'calc(10vw + 50px)',
+        marginTop: 'calc(10vw + 150px)',
+        [theme.breakpoints.down('1300')]: {
+            marginTop: 'calc(20vw + 50px)',
+            right: 'calc(2vw + 10px)'
         },
-        [theme.breakpoints.down('1000')]: {
-            margin: '-300px 0px 0px 600px',
-            width: "406", 
-            height: "511"
-            // marginTop: '89px',
-            // backgroundColor: 'blue'
+        [theme.breakpoints.down('1025')]: {
+            marginTop: 'calc(25vw + 50px)',
+            marginRight: 'calc(8vw - 100px)'
         },
         [theme.breakpoints.down('769')]: {
-            margin: '-400px 0px 0px 500px',
-            // marginTop: '89px',
-            // backgroundColor: 'blue'
+            marginTop: 'calc(40vw - 100px)',
+            marginRight: 'calc(20vw - 100px)'
+        },
+        [theme.breakpoints.down('426')]: {
+            marginTop: 'calc(40vw + 200px) ',
+            left: 'calc(20vw + 50px)',
         },
     },
-    starsStyle: {
-        position: 'relative',
-        width: "406", 
-        height: "511",
-        [theme.breakpoints.down('xl')]: {
-            margin: '-435px 0px 0px 60px',
-        },
-        [theme.breakpoints.down('960')]: {
-            margin: '-370px 0px 0px -50px',
-            width: "361", 
-            height: "454",
-        },
-        [theme.breakpoints.down('768')]: {
-            width: "361", 
-            height: "454",
-            [theme.breakpoints.down('600')]: {
-                margin: '-300px 0px 0px 60px',
-            },
-            [theme.breakpoints.down('426')]: {
-                margin: '-320px 0px 0px 0px',
-            }
-        },
-    }
 }));
 
 export default function ThankYou() {
@@ -88,12 +124,22 @@ export default function ThankYou() {
 
     return (
 
-        <Grid container alignItems="center" justify="center">
+        <Grid className={classes.container} alignItems="center" justify="center">
 
-            <Grid className={classes.blobStyle}>
+            <Grid item xs={12} sm={6} lg={8} xl={12} className={classes.starsStyle}>
+                <SVGLoad image={Stars} alt="sponsorStars" />
+            </Grid>
+
+            <Grid item xs={5} sm={4} lg={8} xl={12} className={classes.handsStyle}>
+                <SVGLoad image={Hands} alt="sponsorStars" />
+            </Grid>
+
+            <Grid item xs={12} sm={12} lg={8} xl={10} className={classes.blobStyle}>
+
                 <svg viewBox="0 0 1056 344" fill="none" xmlns="http://www.w3.org/2000/svg">
+
                     <path fill-rule="evenodd" clip-rule="evenodd" d="M692.553 273.274C834.991 166.805 1067.12 185.273 1055.09 95.7201C1044.7 18.4531 382.414 -27.4718 259.28 20.5836C144.282 65.4637 82.5093 82.6956 20.5314 184.331C-35.1913 275.709 37.0658 300.949 99.3671 322.635C175.328 349.075 566.737 367.318 692.553 273.274Z" fill="#E8D1FF" fill-opacity="0.4"/>
-                
+        
                     <foreignObject x='434' y='17' width="140" height="44" className={classes.headerText}> 
                         Thank you!
                     </foreignObject>
@@ -105,27 +151,29 @@ export default function ThankYou() {
                     <foreignObject x='168' y='155' className={classes.bodyText} width="719" height="120">
                         Our work would not have been possible without the overwhelming positive and creative community of Civictech Toronto. Thank you to the amazing volunteers, sponsors, and community partners who help make this space a reality so projects like ours can begin.
                     </foreignObject>
+
+                </svg>
+
+                <svg className={classes.mobileBlobStyle} height="360" viewBox="0 0 360 185" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" clip-rule="evenodd" d="M281.222 146.894C358.047 89.4647 483.251 99.4261 476.757 51.1214C471.154 9.44378 113.946 -15.328 47.5323 10.593C-14.4929 34.8012 -47.8104 44.096 -81.2387 98.9181C-111.293 148.207 -72.3207 161.822 -38.7179 173.519C2.25214 187.78 213.362 197.621 281.222 146.894Z" fill="#E8D1FF" fill-opacity="0.4"/>
+                
+                    <foreignObject x='120' y='-60' width="140" height="44" className={classes.headerText}> 
+                        Thank you!
+                    </foreignObject>
+
+                    <foreignObject x='20' y='-15' className={classes.bodyText} width="330" height="66">
+                        We are exceedingly grateful to our sponsors, who allow us to continue to offer life-changing tech mentoring through our program.
+                    </foreignObject>
+
+                    <foreignObject x='20' y='65' className={classes.bodyText} width="330" height="300">
+                        Our work would not have been possible without the overwhelming positive and creative community of Civictech Toronto. Thank you to the amazing volunteers, sponsors, and community partners who help make this space a reality so projects like ours can begin.
+                    </foreignObject>
+                
                 </svg>
 
             </Grid>
 
-            <Grid container alignItems="center" justify="center">
-            
-                <Grid className={classes.starsStyle}>
-                    <SVGLoad image={Stars} alt="sponsorStars" />
-                </Grid>
-
-            </Grid>
-
-            <Grid container alignItems="center" justify="center">
-            
-                <Grid className={classes.handsStyle}>
-                    <SVGLoad image={Hands} alt="sponsorHands" />
-                </Grid>
-
-            </Grid>
-
         </Grid>
-
+        
     )
 };
