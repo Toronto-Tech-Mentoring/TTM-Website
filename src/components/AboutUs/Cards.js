4b45d433970f7d3a42d7cae870938797ee8105c1
David Ghazi <davidaghazi@gmail.com>
2021-01-19 23:30:52 -0500

    fixed background blobs for images in cards section

--- src/components/AboutUs/Cards.js
+++ src/components/AboutUs/Cards.js
@@ -4,9 +4,9 @@ import Card from "@material-ui/core/Card"
 import CardContent from "@material-ui/core/CardContent"
 import Grid from "@material-ui/core/Grid"
 
-import Background1 from "../../images/about-us/How-are-we-different-section/background1.svg"
-import Background2 from "../../images/about-us/How-are-we-different-section/background2.svg"
-import Background3 from "../../images/about-us/How-are-we-different-section/background3.svg"
+import Background1 from "../../images/aboutUsBackground/background1.svg"
+import Background2 from "../../images/aboutUsBackground/background2.svg"
+import Background3 from "../../images/aboutUsBackground/background3.svg"
 import GoalSVG from "../../images/about-us/How-are-we-different-section/goals.svg"
 import SkillSVG from "../../images/about-us/How-are-we-different-section/conversation.svg"
 import SupportSVG from "../../images/about-us/How-are-we-different-section/support.svg"
@@ -69,8 +69,6 @@ const useStyles = makeStyles(theme => ({
 
 export default function Cards() {
   const classes = useStyles()
-  const card2Background = Background2
-  const card3Background = Background3
 
   return (
     <Grid container alignItems="center" justify="center">
@@ -100,7 +98,7 @@ export default function Cards() {
           <p className={classes.title}>One-on-one skills development</p>
           <div
             className={classes.imgDiv}
-            style={{ backgroundImage: { card2Background } }}
+            style={{ backgroundImage: `url(${Background2}` }}
           >
             <SkillSVG
               className={classes.innerImage}
@@ -126,16 +124,16 @@ export default function Cards() {
           <p className={classes.title}>Wrap-around supports</p>
           <div
             className={classes.imgDiv}
-            style={{ backgroundImage: { card3Background } }}
+            style={{ backgroundImage: `url(${Background3}` }}
           >
             <SupportSVG
               className={classes.innerImage}
-              style={{ top: "44px", zIndex: "3", left: "60px" }}
+              style={{ top: "15px", zIndex: "3", left: "60px" }}
               alt="supports"
             />
             <Shiny2SVG
               className={classes.innerImage}
-              style={{ top: "33px", zIndex: "2", right: "60px" }}
+              style={{ top: "31px", zIndex: "2", right: "60px" }}
               alt="shiny"
             />
           </div>
