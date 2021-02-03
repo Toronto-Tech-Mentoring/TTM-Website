d3f267fa5986f5ccdab6fb09c503aa63658e00ad
David Ghazi <davidaghazi@gmail.com>
2021-02-03 00:28:37 -0500

    new why tech section for landing page

---
+++ src/components/LandingPage/WhyTech.js
@@ -0,0 +1,20 @@
+import React from "react"
+import Grid from "@material-ui/core/Grid"
+
+const containerStyle = {
+  width: "100%",
+  height: "417px",
+}
+
+export default function WhyTech() {
+  const header = "Why Tech?"
+  //   const body = "Our world..."
+  //   const header2 = "Our Impact"
+  //   const subHeader = "365"
+  //   const body2 = "Hours of mentoring through our program"
+  return (
+    <Grid style={containerStyle}>
+      <h1>Hello</h1>
+    </Grid>
+  )
+}
