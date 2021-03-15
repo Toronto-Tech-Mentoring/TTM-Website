a88292b6d37be5cf7f4c4289e4f1e1e04a8ba48c
David Ghazi <davidaghazi@gmail.com>
2021-03-14 22:08:57 -0400

    completing section responsiveness

--- src/components/Sponsors/ThankYouSection.js
+++ src/components/Sponsors/ThankYouSection.js
@@ -10,10 +10,44 @@ const useStyles = makeStyles(theme => ({
   container: {
     height: "488px",
     width: "100%",
+    [theme.breakpoints.down("769")]: {
+      height: "350px",
+    },
+    [theme.breakpoints.down("600")]: {
+      height: "600px",
+    },
   },
   backgroundBlob: {
-    position: "absolute",
-    margin: "59px 0px 0px 77px",
+    marginLeft: "calc(5vw + 10px)",
+    marginTop: "calc(4vw + 10px)",
+    width: "1050px",
+    height: "579px",
+    [theme.breakpoints.down("600")]: {
+      visibility: "hidden",
+      height: "0px",
+      width: "0px",
+    },
+  },
+  backgroundBlobMobile: {
+    visibility: "visible",
+    margin: "0px 0px 0px 0px",
+    height: "0",
+    width: "0",
+    [theme.breakpoints.down("600")]: {
+      width: "100%",
+      height: "619px",
+      visibility: "visible",
+      position: "relative",
+    },
+  },
+  blobContainer: {
+    [theme.breakpoints.down("769")]: {
+      width: "738px",
+      height: "406px",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+    },
   },
   headerText: {
     fontFamily: "Josefin Sans",
@@ -24,6 +58,16 @@ const useStyles = makeStyles(theme => ({
     display: "flex",
     alignItems: "center",
     color: "#000000",
+    [theme.breakpoints.down("769")]: {
+      width: "245px",
+      height: "38px",
+      fontSize: "22px",
+    },
+    [theme.breakpoints.down("600")]: {
+      width: "245px",
+      fontSize: "20px",
+      lineHeight: "32px",
+    },
   },
   bodyText: {
     fontFamily: "Poppins",
@@ -34,10 +78,36 @@ const useStyles = makeStyles(theme => ({
     display: "flex",
     align: "center",
     color: "#000000",
+    [theme.breakpoints.down("769")]: {
+      width: "534px",
+      height: "138px",
+      fontSize: "16px",
+    },
+    [theme.breakpoints.down("600")]: {
+      width: "330px",
+      height: "169px",
+      fontSize: "14px",
+      lineHeight: "24px",
+    },
   },
   handsStyle: {
     position: "absolute",
-    margin: "240px 0px 0px 715px",
+    marginLeft: "calc(46vw + 50px)",
+    marginTop: "calc(20vw + 40px)",
+    // marginTop: "240px",
+    // marginLeft: "792px",
+    width: "500px",
+    height: "287px",
+    [theme.breakpoints.down("769")]: {
+      width: "40%",
+      marginLeft: "calc(50vw + 80px)",
+      marginTop: "calc(20vw + 60px)",
+    },
+    [theme.breakpoints.down("600")]: {
+      width: "0",
+      height: "0",
+      visibility: "hidden",
+    },
   },
 }))
 
@@ -46,10 +116,20 @@ export default function ThankYou() {
   return (
     <>
       <Grid className={classes.container}>
-        <Grid>
+        <Grid className={classes.handsStyle}>
+          <SVGLoad image={Hands} alt="sponsorStars" />
+        </Grid>
+        <Grid
+          className={classes.blobContainer}
+          container
+          item
+          xl={7}
+          lg={9}
+          md={10}
+          sm={12}
+          xs={12}
+        >
           <svg
-            width="1050"
-            height="579"
             viewBox="0 0 1050 579"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
@@ -117,10 +197,76 @@ export default function ThankYou() {
               Follow us for updates and share with your networks.
             </foreignObject>
           </svg>
-        </Grid>
 
-        <Grid className={classes.handsStyle}>
-          <SVGLoad image={Hands} alt="sponsorStars" />
+          <svg
+            viewBox="0 0 360 619"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className={classes.backgroundBlobMobile}
+          >
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M223.36 522.536C248.833 271.44 519.477 197.373 512.881 101.482C507.191 18.7471 144.384 -30.4279 76.9297 21.0283C13.9325 69.0846 -19.9071 87.5359 -53.8594 196.364C-84.3849 294.209 -21.6773 545.894 12.452 569.114C54.0642 597.425 126.137 687.082 223.36 522.536Z"
+              fill="#E8D1FF"
+              fill-opacity="0.4"
+            />
+            <foreignObject
+              x="20"
+              y="47"
+              width="140"
+              height="42"
+              className={classes.headerText}
+            >
+              Thank you!
+            </foreignObject>
+
+            <foreignObject
+              x="20"
+              y="90"
+              width="672"
+              height="53"
+              className={classes.bodyText}
+            >
+              We are exceedingly grateful to our sponsors, who allow us to
+              continue to offer life- changing tech mentoring through our
+              program.
+            </foreignObject>
+
+            <foreignObject
+              x="20"
+              y="168"
+              width="672"
+              height="111"
+              className={classes.bodyText}
+            >
+              Our work would not have been possible without the overwhelmingly
+              positive and creative community of Civitech Toronto. Thank you to
+              the amazing volunteers, sponsors and community partners who help
+              make this space a reality so projects like ours can begin.
+            </foreignObject>
+
+            <foreignObject
+              x="20"
+              y="414"
+              width="298"
+              height="42"
+              className={classes.headerText}
+            >
+              Help promote our work
+            </foreignObject>
+
+            <foreignObject
+              x="20"
+              y="451"
+              width="463"
+              height="27"
+              className={classes.bodyText}
+              style={{ width: "259px" }}
+            >
+              Follow us for updates and share with your networks.
+            </foreignObject>
+          </svg>
         </Grid>
       </Grid>
 
