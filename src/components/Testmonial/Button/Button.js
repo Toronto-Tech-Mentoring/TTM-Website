e0f3053a3e62838c0139e10e46da7e48c443283b
David Ghazi <davidaghazi@gmail.com>
2021-04-13 22:28:32 -0400

    updating with master

--- src/components/Testmonial/Button/Button.js
+++ src/components/Testmonial/Button/Button.js
@@ -2,7 +2,6 @@ import React from "react"
 import { makeStyles } from "@material-ui/core/styles"
 
 const useStyles = makeStyles(theme => ({
-<<<<<<< HEAD
   testimonialButton: {
     backgroundColor: "#873FE2",
     borderRadius: "30px",
@@ -24,42 +23,9 @@ const useStyles = makeStyles(theme => ({
     },
   },
 }))
-=======
-    testimonialButton: {
-        backgroundColor: "#873FE2",
-        borderRadius: "30px",
-        width: "calc(18vw + 122px)",
-        minWidth: "116px",
-        height: "calc(1.5vw + 28px)",
-        minHeight: "24px",
-        fontSize: "18px",
-        color: "white",
-        borderStyle: "none",
-        fontFamily: "Poppins",
-        marginLeft: "15vw",
-        [theme.breakpoints.between("sm","md")]: {
-          marginLeft: "3vw",
-        },
-        [theme.breakpoints.between("xs","sm")]: {
-          marginLeft: "3vw",
-        },
-
-
-    }
-  }))
->>>>>>> master
 
 export default function TestimonialButton({ buttonText }) {
   const classes = useStyles()
 
-<<<<<<< HEAD
   return <button className={classes.testimonialButton}>{buttonText}</button>
-=======
-
-  return (
-    <button className={classes.testimonialButton} >
-      {buttonText}
-    </button>
-  )
->>>>>>> master
 }
