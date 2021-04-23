b35b345cc84d44dff4fbd7897f21097473a5d163
Brice Boutet <boutetlb@gmail.com>
2021-04-23 02:58:28 -0400

    Correcting chetoohleft margins

--- src/components/Cheetos/cheetohStyle.js
+++ src/components/Cheetos/cheetohStyle.js
@@ -1,24 +1,23 @@
 import { makeStyles } from "@material-ui/core/styles"
 
 const useStyles = makeStyles(theme => ({
-  cheetohRight: {
-    float: "right",
-    transform: "rotate(330deg)",
-  },
-  cheetohLeft: {
-    margin: "-2em 10px 0px 0px",
-    float: "left",
-    transform: "rotate(330deg)",
-  },
-  cheetohRightMirrored: {
-    float: "right",
-    transform: "scaleY(-1) rotate(315deg)",
-  },
-  cheetohLeftMirrored: {
-    marginLeft: "calc(18vw - 62.5px)",
-    float: "left",
-    transform: "scaleY(-1) rotate(315deg)",
-  },
+    cheetohRight: {
+        float: "right",
+        transform: "rotate(330deg)",
+    },
+    cheetohLeft: {
+        margin: "-2em 10px 0px 0px",
+        float: "left",
+        transform: "rotate(330deg)",
+    },
+    cheetohRightMirrored: {
+        float: "right",
+        transform: "scaleY(-1) rotate(315deg)",
+    },
+    cheetohLeftMirrored: {
+        float: "left",
+        transform: "scaleY(-1) rotate(315deg)",
+    },
 }))
 
 export { useStyles }
