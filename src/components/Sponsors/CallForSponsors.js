66be69dc53bcd38c2505de63e5b3a8c231e2d51a
Brice Boutet <boutetlb@gmail.com>
2021-04-28 02:07:06 -0400

    Top skyline component changed on sponsor page

--- src/components/Sponsors/CallForSponsors.js
+++ src/components/Sponsors/CallForSponsors.js
@@ -6,25 +6,27 @@ import Content from "../DecoratedText/DecoratedText"
 import CheetoLeft from "../../images/cheetohs/cheetohLeft.svg"
 import CheetoRight from "../../images/cheetohs/cheetohRight.svg"
 import { useStyles } from "../Cheetos/cheetohStyle"
-import { SkylineHeader as SkylineHeaderSVG } from "../SkylineSVG"
+import { firstSectionStyles } from "../../styles/firstSectionStyles"
+import SkylineTop from "../../components/SkylineTop"
 
 export default function CallForSponsors() {
   const classes = useStyles()
+  const styles = firstSectionStyles
   const headerText = "Sponsor Us"
   const content =
     "Toronto Tech Mentoring is an entirely volunteer-run organization. All donations go directly to providing resources to help deliver our tech mentoring program. Help us continue to give young people a chance to access tech skills and change their lives."
 
   return (
-    <div>
+    <div style={styles.container}>
       <CheetoLeft
         className={classes.cheetohLeftMirrored}
         style={{ "margin-top": "-1em" }}
         alt="cheetohLeft"
       />
       <TextBox
-        style={{
-          "margin-top": "135px",
-          "margin-right": "calc(37vw - 100px)",
+         style={{
+          "margin-left": "10px",
+          "margin-right": "calc(46.5vw - 151px)",
           "padding-right": "0",
         }}
       >
@@ -42,7 +44,7 @@ export default function CallForSponsors() {
           alt="cheetohRight"
         />
       </TextBox>
-      <SkylineHeaderSVG />
+      <SkylineTop />
     </div>
   )
 }
