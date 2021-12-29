6026191fc1ddb6cc60bb1167b833dc53bb519caf
Rishi Khan <rishi.rkhan@gmail.com>
2021-12-28 21:26:14 -0500

    Changed gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,7 +4,7 @@ exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
       module: {
         rules: [
           {
-            test: /bad-module/,
+            test: /webfontloader/,
             use: loaders.null(),
           },
         ],
